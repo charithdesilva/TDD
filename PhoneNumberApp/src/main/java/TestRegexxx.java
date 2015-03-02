6f4ccd4cd567bf263ddfc17f50e4a045e98db682
Charith De Silva <cdesilva@DEAMELHQGR622.services.esriaustralia.com.au>
2015-03-02 18:09:03 +1100

    commit #3

--- PhoneNumberApp/src/main/java/TestRegexxx.java
+++ PhoneNumberApp/src/main/java/TestRegexxx.java
@@ -7,15 +7,15 @@ public class TestRegexxx {
     
     public static void main(String[] args) {
 	
-	String source = ",CALLMED,ADAD,ASAS,";
+	String source = "AASAS,CALLMED,ADAD,ASAS,";
 	
 //	Pattern p = Pattern.compile(("^+(,\\w+,)?(CALL)(,\\w+,).*"));
-	Pattern p = Pattern.compile(("([CALL])"));
+	Pattern p = Pattern.compile(("^.*,(CALL\\w+),.*$"));
 	Matcher m = p.matcher(source);
 	
         if(m.find()) {
             System.out.println("Matching group found 0: " +m.group(0));
-//            System.out.println("Matching group found 1: " +m.group(1));
+            System.out.println("Matching group found 1: " +m.group(1));
 //            System.out.println("Matching group found 2: " +m.group(2));
 //            System.out.println("Matching group found 3: " +m.group(3));
         }
--- PhoneNumberApp/src/main/java/com/aconex/codechallenge/pojos/Dictionary.java
+++ PhoneNumberApp/src/main/java/com/aconex/codechallenge/pojos/Dictionary.java
@@ -16,19 +16,28 @@ public class Dictionary {
     
     private Map<String, DictionaryWord> dictionaryMap = new HashMap<>();
     private String filePath = null;
+    private StringBuffer dictionaryText = new StringBuffer();
+    
+    public Dictionary() {
+	dictionaryText.append(",");
+    }
     
     public void addWord(DictionaryWord word) {
 	dictionaryMap.put(word.getWord(), word);
+	dictionaryText.append(word.getWord()+",");
     }
     
     public void checkWord(String wordStr) {
 	
 	DictionaryWord word = dictionaryMap.get(wordStr.toUpperCase());
 	
-	Pattern p = Pattern.compile(("(CALL)?(\\w+)"));
+	Pattern p = Pattern.compile("^.*,("+word.getWord()+"\\w+),.*$");
 	Matcher m = p.matcher(word.getWord());
 	
         if(m.find()) {
+            
+            
+            
             System.out.println("Matching group found 0: " +m.group(0));
             System.out.println("Matching group found 1: " +m.group(1));
             System.out.println("Matching group found 2: " +m.group(2));
